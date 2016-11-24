292014fbc95d88a00ad8132f8a78166350eb4625
Arash <arash.khoeini@gmail.com>
2016-11-24 21:34:20 +0330

    Fixes output bug

--- FuzzyController.m
+++ FuzzyController.m
@@ -1,4 +1,4 @@
-temp_input = [5 10];
+temp_input = [25 10];
 d_temp_input = [-2 4];
 
 %Defining Membership functions
@@ -23,11 +23,11 @@ fan_medium = [zeros(1,400) l r zeros(1,200)];
 fan_very_high = [zeros(1,800) l];
 
 %Defining Rules
-rules = [very_low very_low medium medium medium ;
-   very_low very_low medium medium medium ; 
-   medium medium medium medium medium; 
-   very_high very_high high high high ;
-   very_high very_high high high high];
+rules = [fan_low fan_low fan_medium fan_medium fan_medium ;
+   fan_low fan_very_low fan_medium fan_medium fan_medium ; 
+   fan_medium fan_medium fan_medium fan_medium fan_medium; 
+   fan_very_high fan_very_high fan_high fan_high fan_high ;
+   fan_very_high fan_very_high fan_high fan_high fan_high];
 
 %Fuzzification temp_input
 temp_input(1) = temp_input(1) * 10;
@@ -55,11 +55,11 @@ temp_active = [ all(t_norm(very_low, temp_input) == 0)==0 all(t_norm(low, temp_i
 d_temp_active = [ all(t_norm(very_low, d_temp_input) == 0)==0 all(t_norm(low, d_temp_input)==0)==0 ...
     all(t_norm(medium, d_temp_input)==0)==0 all(t_norm(high, d_temp_input)==0)==0 all(t_norm(very_high, d_temp_input)==0)==0 ];
 memberships = [[very_low] ; [low] ; [medium] ; [high] ; [very_high]];
-output = zeros(1,2500);
+output = zeros(1,1000);
 for i=1:5
     for j=1:5
         if(temp_active(i) && d_temp_active(j))
-            inf_result = inference(i,j,temp_input , d_temp_input , rules(i,((j-1)*2500)+1:j*2500) , memberships );
+            inf_result = inference(i,j,temp_input , d_temp_input , rules(i,((j-1)*1000)+1:j*1000) , memberships );
             output = [output ; inf_result];
             %plot(output)
         end
